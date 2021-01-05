9ca2aa659779becce894f4a91160ffd8fd929a9c
neporood <neporood@gmail.com>
2021-01-05 02:15:36 -0600

    update fp & desc to Jan 2021 (RQ1A.210105.003)

--- scorpion.mk
+++ scorpion.mk
@@ -38,12 +38,12 @@ PRODUCT_MODEL := Pixel 3 XL
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="crosshatch" \
-    PRIVATE_BUILD_DESC="crosshatch-user 11 RQ1A.201205.003 6906706 release-keys"
+    PRIVATE_BUILD_DESC="crosshatch-user 11 RQ1A.210105.003 7005429 release-keys"
 
-BUILD_FINGERPRINT := "google/crosshatch/crosshatch:11/RQ1A.201205.003/6906706:user/release-keys"
+BUILD_FINGERPRINT := "google/crosshatch/crosshatch:11/RQ1A.210105.003/7005429:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/crosshatch/crosshatch:11/RQ1A.201205.003/6906706:user/release-keys
+    ro.build.fingerprint=google/crosshatch/crosshatch:11/RQ1A.210105.003/7005429:user/release-keys
 
 # Active Edge
 PRODUCT_PACKAGES += \
